e33379bb85d4d06adc9a3d93577fc3383f1a97ae
Athul Krishna <70716078+Athul-Krishna-998@users.noreply.github.com>
2021-07-03 09:50:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,134 @@
+const number=document.querySelectorAll(".number");
+const operand=document.querySelectorAll(".operation");
+const input=document.querySelector(".previous");
+const output=document.querySelector(".current");
+const clear=document.querySelector(".ac");
+const equals=document.querySelector(".equals");
+const deletion=document.querySelector(".delete");
+
+let currentnum="";
+let oldnum="";
+let currentoperation="";
+
+number.forEach(function(numbers)
+{
+     numbers.addEventListener('click',function()
+    {
+                addNumber(numbers.innerText);
+                updateDisplay();
+    });
+})
+
+operand.forEach(function(operator)
+{
+     operator.addEventListener('click',function()
+    {
+                addOperation(operator.innerText);
+                updateDisplay();
+    });
+})
+
+equals.addEventListener('click',compute);
+clear.addEventListener('click',clearscreen);
+deletion.addEventListener('click',removeal);
+
+function clearscreen()
+{
+    currentnum="";
+    oldnum="";
+    currentoperation="";
+    updateDisplay();
+}
+
+function removeal()
+{
+    currentnum=currentnum.toString().slice(0, currentnum.length - 1);
+    updateDisplay();
+}
+
+function addNumber(num)
+{
+   num=num.toString();
+   if(currentnum.includes('.') && num==='.') return;
+
+   currentnum+=num;
+}
+
+function addOperation(item)
+{
+  if(currentnum==='') return;
+  if(oldnum!=='')
+  {
+      compute();
+  }
+  currentoperation=item;
+  oldnum=currentnum;
+  currentnum="";
+}
+
+function compute()
+{
+    let result;
+    if(currentnum==='' && oldnum==='') return;
+    let num1=parseFloat(oldnum);
+    let num2=parseFloat(currentnum);
+    switch(currentoperation)
+    {
+        case '+' :
+         {
+            result=num1+num2;
+            break;
+         }
+         case '-' :
+         {
+            result=num1-num2;
+            break;
+         }
+         case '÷' :
+         {
+            result=num1/num2;
+            break;
+         }
+         case '*' :
+         {
+            result=num1*num2;
+            break;
+         }
+         default:
+         break;
+    }
+    currentnum=result.toString();
+    oldnum="";
+    currentoperation="";
+    updateDisplay();
+}
+function commavalue(word)
+{
+  const wordno=word.toString();
+  integer=parseFloat(wordno.split('.')[0]);
+  decimal=wordno.split('.')[1];
+  let intdisplay='';
+  if(isNaN(integer))
+  {
+      intdisplay='';
+  }
+  else{
+    intdisplay=integer.toLocaleString('en',{maximumFractionDigits:0})
+  }
+  if(decimal!=null)
+  {
+     return `${intdisplay}.${decimal}`;
+  }
+  else
+  {
+      return intdisplay;
+  }
+}
+function updateDisplay()
+{
+    output.innerText=commavalue(currentnum);
+    if(currentoperation!==null)
+    {
+    input.innerText=`${commavalue(oldnum)} ${currentoperation}`;
+    }
+}
